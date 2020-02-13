d6efec82663263206e9e3c78aa5ce16a726b8325
yadrit <yadritig@gmail.com>
2020-02-13 20:31:54 +0200

    initial commit - create django project

---
+++ check.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+python src/manage.py check
+flake8 src
+python src/manage.py test
+pip check
---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,8 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=w%t(2c!m=6ev5c7yi6s7vf8yknq7x!7-d1xk9vf1&hqrtt1oc'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
